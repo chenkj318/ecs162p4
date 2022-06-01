96f9f9b616356aa3fa788aa0dcd4adf6863ef4c2
KejingChen <60248229+chenkj318@users.noreply.github.com>
2022-06-01 00:08:19 +0000

    start working on chart

---
+++ src/Buchart.jsx
@@ -0,0 +1,17 @@
+// import React, {useState} from 'react';
+// import './App.css';
+// import MonthPicker from "simple-react-month-picker";
+// import {Bar} from "react-chartjs-2"
+// import {Chart} from 'chart.js';
+
+// function Buchart(){
+
+//   return(
+//     <div>
+//       testing
+//     </div>
+
+//   )
+// }
+
+// export default Buchart;
